e860386fcf064fa206eab572ac200f1f92a8c5a8
thabrita <britothais30@gmail.com>
2022-09-20 21:45:42 -0300

    exercício arrays

---
+++ aula02/index.js
@@ -0,0 +1,40 @@
+// const alunas = ['Thaís', 'Verônica', 'Carolina']
+
+// console.log(alunas.length)
+// console.log('ARRAY ORIGINAL ', alunas)
+
+// alunas.push('Drika', 'Vera', 'Paula', 'Nadja')
+
+// console.log(alunas.length)
+// console.log('ARRAY MODIFICADO', alunas)
+
+// alunas.pop()
+// alunas.pop()
+
+// console.log(alunas.length)
+// console.log('REMOVENDO VALORES', alunas)
+
+// const notas = [1, 2, 3]
+
+// notas.forEach((nomes) => {
+//     console.log(nomes)
+// })
+
+
+// const linguagens = ['JS', 1, 2, 3, 'HTML', 'CSS'] 
+
+// linguagens.forEach((linguagens) => {
+//     if(isNaN(linguagens)) {
+//         console.log(linguagens)
+//     }
+// })
+
+const alunas = ['Thaís', 'Verônica', 'Carolina']
+
+const cursos = ['JS', 'CSS', 'HTML']
+
+const pretalab = [alunas, cursos]
+
+const cursosLivres = ['MinasProgram', pretalab, 'Programaria']
+
+console.log(cursosLivres[1][1][2])
